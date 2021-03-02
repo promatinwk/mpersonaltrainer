a059fcbff18245854f30c4194179bfb011db2456
matinwk <promatinwk@gmail.com>
2021-03-02 19:26:09 +0000

    test slider
    it's not working, but i think that i know why.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const navBurger = document.querySelector('.nav__burger');
 const mobileMenu = document.querySelector('.nav__menu');
 const navLinks = document.querySelectorAll('.nav__link');
+// const slider = document.querySelector('.certificates__carousel__track');
+// const slides = Array.from(document.querySelectorAll('.certificates__carousel__slide'));
+// let dragging = false;
+// let startPos = 0;
+// let currentTranslate = 0;
+// let previousTranslate = 0;
+// let animationID = 0;
+// let currentIndex = 0;
 
 const showMenu = ()=>{
     mobileMenu.classList.toggle('nav__menu--active');
@@ -52,14 +60,87 @@ const smoothScroll = (event)=>{
     scroll(targetId, 2000);
 }
 
+// slides.forEach((slide, index)=>{
+//     const slideImage = slide.querySelector('img');
+//     slideImage.addEventListener('dragstart', (e)=>e.preventDefault())
+
+//     //touchEvents
+//     slide.addEventListener('touchstart', touchStart(index))
+//     slide.addEventListener('touchend', touchEnd)
+//     slide.addEventListener('touchmove', touchMove)
+//     //mouseEvents
+//     slide.addEventListener('mousedown', touchStart(index))
+//     slide.addEventListener('mouseup', touchEnd)
+//     slide.addEventListener('mouseleave', touchEnd)
+//     slide.addEventListener('mousemove', touchMove)
+// })
+
+
+//disable context menu
+// window.oncontextmenu = function (event){
+//     event.preventDefault();
+//     event.stopPropagation(); 
+//     return false;
+// }
+
+// function touchStart(index){
+//     return function (event){
+//         console.log('start');
+//         startPos = getPositionX(event)
+//         currentIndex = index;
+//         dragging = true;
+//         animationID = requestAnimationFrame(animate)
+//     }
+// }
+
+// function touchEnd(){
+//         dragging = false;
+//         const movedBy = currentTranslate - previousTranslate 
+
+//         if(movedBy < -100 && currentIndex < slides.length - 1){
+//         currentIndex += 1;
+//         }else if( movedBy > 100 && currentIndex > 0){
+//             currentIndex =-1;
+//         }
+
+//         setPositionByIndex()
+
+//         cancelAnimationFrame(animationID);
+// }
+
+// function touchMove(event){
+//         if (dragging){
+//             const currentPosition = getPositionX(event);
+//             currentTranslate = previousTranslate + currentTranslate - startPos;
+//         }
+// }
+
+// function getPositionX(event){
+//    return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+// }
+
+// function animate(){
+//     setSliderPosition()
+//     if (dragging)requestAnimationFrame(animate)
+// }
+
+
+// function setSliderPosition(){
+//     slider.style.transform = `translateX(${currentTranslate}px)`;
+// }
+
+// function setPositionByIndex(){
+//     currentTranslate = currentIndex * -window.innerWidth;
+//     previousTranslate = currentTranslate;
+//     setSliderPosition();
+// }
+
+
+
+
+
 
 
 
 scrollWebsite()
-
-
-
-
-
-
 navBurger.addEventListener('click', showMenu)
